a66465e3edbc9da3b222ddfd599f031cc92305a3
zalessky <zalessky@gmail.com>
2026-03-13 00:21:23 +0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,25 +41,23 @@ const Header = () => {
         </div>
 
         {/* Logo Right in Yellow Block with Slant */}
-          <div className="hidden md:flex h-full items-start relative z-20">
-            {/* Фикс: убрали margin и добавили жесткое позиционирование для фона */}
+          <div className="hidden md:flex h-full relative z-20">
+            {/* Желтый фон со скосом: жестко привязан к левому краю этого блока */}
             <div
-              className="absolute inset-y-0 bg-brand-yellow z-0"
+              className="absolute inset-y-0 bg-brand-yellow shadow-[-20px_0_20px_rgba(0,0,0,0.2)]"
               style={{ 
-                left: '-40px', // Нахлест на черный блок
-                right: '0',
-                clipPath: 'polygon(40px 0, 100% 0, 100% 100%, 0 100%)' 
+                left: '-60px', // Глубокий нахлест, чтобы не было щели
+                right: '-100vw', // Уходит в бесконечность вправо
+                clipPath: 'polygon(60px 0, 100% 0, 100% 100%, 0 100%)' 
               }}
             ></div>
-            {/* Расширитель вправо до края экрана */}
-            <div className="absolute left-full top-0 bottom-0 bg-brand-yellow w-[100vw] z-0"></div>
-            
-            <div className="relative z-10 flex items-start h-full mr-[100px]">
+          
+            {/* Контейнер логотипа: ширина зафиксирована, чтобы не было "плавания" */}
+            <div className="relative z-10 flex items-start h-full w-[236px] mr-[100px]">
               <img
                 src="/images/rolf.webp"
                 alt="ROLF"
-                className="w-[136px] h-[52px] mt-[14px] flex-shrink-0"
-                style={{ objectFit: 'contain' }}
+                className="w-[136px] h-[52px] mt-[14px] flex-shrink-0 object-contain"
               />
             </div>
           </div>
